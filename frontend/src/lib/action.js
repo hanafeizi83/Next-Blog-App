d5a5df7ff7ecd56c95183a17bee40cd8e2bcad40
hana feizi <hanafaizi83@gmail.com>
2025-11-08 09:52:07 +0330

    fix error in add comment action

--- frontend/src/lib/action.js
+++ frontend/src/lib/action.js
@@ -2,26 +2,26 @@
 import { createCommentApi } from '@/services/commentServices';
 import setCookieOnReq from '@/utils/setCookieOnReq'
 import { cookies } from 'next/headers';
+import { revalidatePath } from 'next/cache';
 
-export async function addComment(prevState, { formData, blogId, parentId }) {
-    const cookieStore = cookies();
+export async function addComment(prevState, { formData, blogId, parentId, blogSlug }) {
+  const cookieStore = cookies();
 
-    const newComment = {
-        text: formData.get('text'),
-        postId: blogId,
-        parentId
-    }
+  const newComment = {
+    text: formData.get('text'),
+    postId: blogId,
+    parentId
+  }
 
-    try {
-        const options = setCookieOnReq(cookieStore);
-        const { message } = await createCommentApi(newComment, options);
-        revalidatePath(`/blogs/[blogSlug]`)
-        return { 
-            message,
-        }
-    } catch (err) {
-        const error = err?.response?.data?.message;
-        return { error }
-    }
+  try {
+    const options = setCookieOnReq(cookieStore);
+    const res = await createCommentApi(newComment, options);
+    revalidatePath(`/blogs/${blogSlug}`);
+    const message = res?.message || 'نظر با موفقیت ثبت شد';
 
+    return { message };
+  } catch (err) {
+    const error = err?.response?.data?.message || 'خطا در ارسال نظر';
+    return { error };
+  }
 }
