91a81df919b6af70ee475a0ebfb11999b8d817f7
hana feizi <hanafaizi83@gmail.com>
2025-11-09 19:41:42 +0330

    change url of image

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [new URL('http://localhost:5000/uploads/**')],
+        remotePatterns: [new URL('https://next-blog-app-tan.vercel.app/uploads/**')],
     }
 };
 
